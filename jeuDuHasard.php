2c5312fee7dd96d4ab4d00c541aece4b39391a44
juldblack <juliendunia@hotmail.com>
2022-03-09 09:08:14 +0100

    Modif jeuduhasard

--- jeuDuHasard.php
+++ jeuDuHasard.php
@@ -3,16 +3,25 @@
     $nav = "jeuDuHasard";
     $numéroGagnant = rand(0,100);
     require "header.php";
-?>
-<?php if(isset($_GET['nombre'])): ?>
-    <?php if ($_GET['nombre'] > $numéroGagnant): ?>
-        Votre nombres est trop grand !
-    <?php elseif ($_GET['nombre'] < $numéroGagnant): ?>
-        Votre nombres est trop petit !
-    <?php else: ?>
-        Vous avez bien trouver le bon nombres <?php echo $numéroGagnant; ?>
-    <?php endif;?>
-<?php endif; ?>
+
+    if(isset($_GET['nombre'])):
+        if (is_numeric($_GET['nombre'])):
+            if (($_GET['nombre'] >= 0) && ($_GET['nombre'] <= 100)):
+                if ($_GET['nombre'] > $numéroGagnant): ?>
+                    Votre nombres est trop grand !
+                <?php elseif ($_GET['nombre'] < $numéroGagnant): ?>
+                    Votre nombres est trop petit !
+                <?php else: ?>
+                    Vous avez bien trouver le bon nombres <?php echo $numéroGagnant; ?>
+                <?php endif;
+            else:?>
+                Veuillez introduir un nombre entre 0 et 100 compris !!
+            <?php endif;
+        else:?>
+            Veuillez introduire un nombre !!!
+        <?php endif; 
+    endif;?>
+
 
 <form action="/coursphp/jeuDuHasard.php" method = "GET">
     <input type="number" name="nombre" placeholder="Entre 0 et 100">
